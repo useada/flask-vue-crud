4a6c8f9b003967a1cfe4967f491f99cbe7e16c7f
a <a@b.c>
2024-11-12 17:24:36 +0800

    klinechart

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeApp from '../views/HomeApp.vue';
+import ChartApp from '../views/ChartApp.vue';
 import UsersIndex from '../views/UserIndex.vue';
 import UserForm from '../components/UserForm.vue';
 import UserDetail from '../components/UserDetail.vue';
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'HomeApp',
     component: HomeApp,
+  },
+  {
+    path: '/chart',
+    name: 'ChartApp',
+    component: ChartApp,
   },
   {
     path: '/users',
